1d722c402939f858cb2ef7365fd13ac73b39c6de
Soumyadip <soumyadipgantait2003@gmail.com>
2023-01-17 01:55:33 +0530

    design index page and integrate with backend

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 require("./db/connection")
 const path = require("path");
-//const hbs = require('hbs');
+const hbs = require('hbs');
 const port = 3000;
 const app = express();
 
-// const staticpath = path.join(__dirname, "../public");
+const staticpath = path.join(__dirname, "../public");
+const views_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 // console.log(staticpath)
 
-// app.use(express.static(staticpath))
+ app.use(express.static(staticpath))
 app.set("view engine","hbs")
+app.set("views",views_path)
+hbs.registerPartials(partials_path)
 //app.set('views',template_path)
 app.get("/",(req,res)=>{
     res.render("index");
@@ -18,3 +22,6 @@ app.get("/",(req,res)=>{
 app.listen(port,(req,res)=>{
         console.log(`port is running on port ${port}`)
 })
+
+
+
